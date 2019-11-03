6991bf0471d2c48c68ce1147b599e0ca22d71b41
yusuke <yusuke@newsdict.jp>
2019-11-03 02:10:37 +0000

    Added DB_HOST to .env

--- docroot/wordpress/wp-config.php
+++ docroot/wordpress/wp-config.php
@@ -41,7 +41,7 @@ define( 'DB_USER', getenv('DB_USER') );
 define( 'DB_PASSWORD', getenv('DB_PASSWORD') );
 
 /** MySQL hostname */
-define( 'DB_HOST', 'mysql' );
+define( 'DB_HOST', getenv('DB_HOST') );
 
 /** Database Charset to use in creating database tables. */
 define( 'DB_CHARSET', 'utf8' );
