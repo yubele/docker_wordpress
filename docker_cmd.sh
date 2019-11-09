6db398dad042601892968980a2c7edd87f3dbf59
yusuke <yusuke@newsdict.jp>
2019-11-09 19:33:29 +0000

    init database

--- docker_cmd.sh
+++ docker_cmd.sh
@@ -1,8 +1,5 @@
 #!/bin/sh
-# Init wordpress and container
 set -ex
-. /.env
-mysql -u$DB_USER -p$DB_PASSWORD -h$DB_HOST -e "create database $DB_NAME IF NOT EXISTS $DB_NAME charset utf8"
 /bin/chown www-data.www-data -R /var/www/wordpress
 nginx -c /etc/nginx/nginx.conf
 php-fpm
