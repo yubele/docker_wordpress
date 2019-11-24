5fac16ad4fbbb6ee29edbab263354b0566a3c427
yusuke <yusuke@newsdict.jp>
2019-11-24 02:02:04 +0000

    bugfix: create sql

--- docker_cmd.sh
+++ docker_cmd.sh
@@ -2,6 +2,7 @@
 # Init wordpress and container
 set -ex
 . /.env
-mysql -u$DB_USER -p$DB_PASSWORD -h$DB_HOST -e "create database $DB_NAME IF NOT EXISTS $DB_NAME charset utf8"
+mysql -u$DB_USER -p$DB_PASSWORD -h$DB_HOST -e "create database IF NOT EXISTS $DB_NAME charset utf8"
+chown www-data.www-data -R /usr/local/src/wordpress
 nginx -c /etc/nginx/nginx.conf
 php-fpm
